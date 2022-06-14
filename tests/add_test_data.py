6eab6bd51f9d016b2bea9177434f57a26581d29c
chathurinda <chathurindaranasinghe@gmail.com>
2022-06-14 10:46:48 +0530

    minor changes to add test data file

--- tests/add_test_data.py
+++ tests/add_test_data.py
@@ -4,31 +4,23 @@ import time
 
 from app.config import settings
 
+while True:
+    try:
+        conn = psycopg2.connect(host=Settings.database_hostname, database=settings.database_name,
+                                user=settings.database_username, password=settings.database_password, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Connection Success! :)")
+        break
+    except Exception as error:
+        print("Connection Failed! :(")
+        print("Error: ", error)
+        time.sleep(2)
 
-if __name__ == '__main__':
-
-    while True:
-        try:
-            conn = psycopg2.connect(host=settings.database_hostname, database=settings.database_name,
-                                    user=settings.database_username, password=settings.database_password, cursor_factory=RealDictCursor)
-            cursor = conn.cursor()
-            print("Connection Success! :)")
-            break
-        except Exception as error:
-            print("Connection Failed! :(")
-            print("Error: ", error)
-            time.sleep(2)
-    
-    query = """
-            COPY "products" (NAME,PRICE,CATEGORY,BRAND,LINK,SHOP,AVAILABILITY,SPEC)
-            FROM 'products_test_data.CSV'
-            DELIMITER ','
-            CSV HEADER
-            """
-    cursor.execute(query)
-    conn.commit()
-        
-
-
-
-
+query = """
+        COPY "products" (NAME,PRICE,CATEGORY,BRAND,LINK,SHOP,AVAILABILITY,SPEC)
+        FROM 'products_test_data.CSV'
+        DELIMITER ','
+        CSV HEADER
+        """
+cursor.execute(query)
+conn.commit()
